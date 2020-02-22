7eebf00e2daae83164739838c20a1446ee295c31
moongua94 <koom6@naver.com>
2020-02-23 00:43:02 +0900

    9093!

--- baekjoon/11718s.c
+++ baekjoon/11718s.c
@@ -2,7 +2,6 @@
 
 
 // <OK code>
-// 한 줄씩 밀려서 나오는데... 이게 맞네? 블로그에 답으로 주로 올라와있음.
 // #include <stdio.h>
 // int main()
 // {
@@ -10,6 +9,7 @@
 // 	while (scanf("%[^\n]\n", a) == 1) // %[^\n] 뒤의 \n는 도대체 뭐야!!!!!
 // 		printf("%s\n", a);
 // }
+/* 한 줄씩 밀려서 나오는데... 이게 맞네? 블로그에 답으로 주로 올라와있음. */
 
 
 // <OK code>
@@ -22,39 +22,7 @@
 // }
 
 
-// <KO code>
-// 왜 틀릴까?...는 아마 read(1, &buf, 1)여서?
-// #include <unistd.h>
-// int main()
-// {
-// 	char buf;
-// 	// int i = 0;
-
-// 	// while (i < 100)
-// 	// {
-// 		// while (read(1, &buf, 1) != -1 && buf != '\n')
-// 		while (read(1, &buf, 1) != -1)
-// 		{
-// 			write(1, &buf, 1);
-// 		}
-// 		// write(1, "\n", 1);
-// 		// i++;
-// 	// }
-// }
-
-
-// <출력초과>
-// #include <unistd.h>
-// int main()
-// {
-// 	char buf;
-// 	while (read(0, &buf, 1) != -1)
-// 		write(1, &buf, 1);
-// }
-
-
 // <OK code>
-// 카톡 센세가 알려주신 방법. 위랑 뭐가 다르지..? 질문할것!!!!!
 // #include <unistd.h>
 // int main()
 // {
@@ -66,29 +34,116 @@
 // 		write(1, &buf, 1);
 // 	}
 // }
+/* 카톡 센세가 알려주신 방법. 위랑 뭐가 다르지..? 질문할것!!!!! */
 
 
-// 하나씩 읽고, 하나씩 쓰는 방법 말고.. 한번에 읽고 쓰는 방법 해보고 싶었는데.
-// 한 번은 되는데, 반복을 못 시키겠다.!!!!! 해결 필요.!!!!!
+// <OK code>
 // #include <stdio.h>
 // int main()
 // {
+// 	char inp;
+// 	inp = getchar();
+// 	while(inp != EOF)
+// 	{
+// 		putchar(inp);
+// 		inp = getchar();
+// 	}
+// }
+/* 드디어 이해했다..! 개행도 바로 읽고 쓰이기 때문에
+반복문은 파일이 끝나기 전까지 계속 돌아가는 것. */
+
+
+// <KO code>
+// #include <stdio.h>
+// int main()
+// {
+// 	int i = 0;
 // 	char str[101];
-// 	scanf("%[^\n]s", str);
-// 	printf("%s\n", str);
+// 	while (i < 100)
+// 	{
+// 		gets(str);
+// 		puts(str);
+// 		// printf("%s\n", str);
+// 		i++;
+// 	}
 // }
 
 
+// <OK code>
+// #include <stdio.h>
+// int main()
+// {
+// 	char str[110];
+// 	int i;
+// 	int j = 0;
+// 	while (j < 100)
+// 	{
+// 		i = 0;
+// 		while (i < 101)
+// 		{
+// 			str[i] = getchar();
+// 			if (str[i] == EOF) // EOF 대신 '\0' 을 해서 틀림...
+// 				return 0; // djm03178 선생님 감사합니다 TT...
+// 			if (str[i] == '\n')
+// 			{
+// 				str[i] = '\0';
+// 				break;
+// 			}
+// 			i++;
+// 		}
+// 		printf("%s\n", str);
+// 		j++;
+// 	}
+// }
+/* 내가 원하던 한줄씩 저장하는 답!!!
+https://www.acmicpc.net/board/view/47061#post
+getchar는 절대 \0을 반환(?)하지 않기 때문에 저 두줄이 문제였음.
+이건 처음 짠거라 i, j 아직 안바꿈. 귀찮아서.. */
+
+
+// <OK code>
 #include <stdio.h>
 int main()
 {
-	char inp;
-	inp = getchar();
-	while(inp != EOF)
+	char str[110];
+	int i = 0;
+	int j;
+	while (i < 100)
 	{
-		putchar(inp);
-		inp = getchar();
+		j = 0;
+		while (j < 101)
+		{
+			scanf("%c", &str[j]);
+			if (str[j] == '\0') // scanf는 null문자를 반환해줘서 이 코드는 맞음.
+				return 0; // 일단 이 두줄이 필요하다는 것도 사실 확신 없었음..
+			if (str[j] == '\n')
+			{
+				str[j] = '\0';
+				break;
+			}
+			j++;
+		}
+		printf("%s\n", str);
+		i++;
 	}
 }
-/* 드디어 이해했다..! 개행도 바로 읽고 쓰이기 때문에
-반복문은 파일이 끝나기 전까지 계속 돌아가는 것. */
+/* getchar와 다르게 scanf는 null문자 반환해줘서 저렇게 해도 됨. 
+i랑 j 급하게 바꾸느라... 안돌아갈수도. */
+
+
+// <OK code>
+// #include <stdio.h>
+// #include <unistd.h>
+// int main()
+// {
+// 	char str[110];
+// 	int size;
+
+// 	while (size = read(0, str, 110))
+// 	{
+// 		write(1, str, size);
+// 		// printf("%d\n", size);
+// 	}
+// }
+/* 개행까지가 저장됨. 개행 다음은 왜 EOF 처리되는거지?
+이부분 해결하자!!! 찾아볼것!!! */
--- baekjoon/9093s.c
+++ baekjoon/9093s.c
@@ -1,31 +1,115 @@
 // https://www.acmicpc.net/problem/9093
 
-// <하다가 포기>
+// <OK code>
 #include <stdio.h>
-#include <string.h>
+
+int is_word(char c)
+{
+	if ('a' <= c && c <= 'z')
+		return 1;
+	if ('A' <= c && c <= 'Z')
+		return 1;
+	return 0;
+}
+
+int ft_count(char *str)
+{
+	int i = 0;
+	int count = 0;
+	while (str[i])
+	{
+		if (is_word(str[i]) == 1 && is_word(str[i+1]) == 0)
+			count++;
+		i++;
+	}
+	return count;
+}
+
+int ft_len(char *str)
+{
+	int i = 0;
+	while (is_word(str[i]) == 1)
+		i++;
+	return i;
+}
+
+void ft_rev(char *str)
+{
+	int word_count;
+	word_count = ft_count(str);
+
+	int	word_len;	
+
+	int i = 0;
+	int j;
+	while(i < word_count)
+	{
+		word_len = ft_len(str);
+		j = 0;
+		while (j < word_len)
+		{
+			printf("%c", str[word_len-j-1]);
+			j++;
+		}
+		printf(" ");
+		while (is_word(*str) == 1)
+			str++;
+		while (is_word(*str) == 0)
+			str++;
+		i++;
+	}
+	printf("\n");
+}
+
 int main()
 {
 	int t;
-	scanf("%d", &t);
-	
-	char buf;
-	int len = 0;
-	while (read(0, &buf, 1) && ++len)
+	scanf("%d", &t); // %d 뒤에 공백 안붙여서 엄청 헤맴.
+
+	char str[1010];
+	int i = 0;
+	int j;
+	while (i < t)
 	{
-		if (buf == '\n')
-			break;
+		j = 0;
+		while (j < 1000)
+		{
+			scanf("%c", &str[j]);
+			if (str[j] == '\0')
+				return 0;
+			if (str[j] == '\n')
+			{
+				str[j] = '\0';
+				break;
+			}
+			j++;
+		}
+		ft_rev(str);
+		i++;
 	}
-	
 }
 
 /*
-도대체 한줄씩 저장을 어떻게 하는것이야... T_T 여기서 계속 막힘. 한글자씩만 해봐서..
-rush02에서 했던거랑 비슷한건가...? 감도 안잡힘.
-*/
+ ./a.out
+2
 
-/*
-<참고>
-https://electro-don.tistory.com/entry/scanf-n-%EA%B4%80%EB%A0%A8
-https://m.blog.naver.com/PostView.nhn?blogId=gksvlfgh0602&logNo=221159664224&proxyReferer=https%3A%2F%2Fwww.google.com%2F
-https://sengsung.tistory.com/71
+i am happy
+i ma yppah
+
+이런 식으로 공백이 하나 나오면서 결과가 t-1개만 나오는 문제로 헤맴.
+원인은 개행문자가 버퍼에 저장되어 있었기 때문.
+(https://www.acmicpc.net/board/view/47071#post)
+
+해결방법은 "%d ". 공백이 아닌 글자를 만날때까지 읽고, 읽은 것은 버린다.
+이 외에도 여러 해결방법이 있다.
+예를 들어 아무 다른 글자에 scanf를 써서 버퍼를 넘겨준다거나..
+(이 방법은 해보진 않아서 할수 있을진 모르겠다.)
+
+어쨌거나 문제와 해결 키워드를 알았으니, 공부해보자.
+아직 문자열을 입, 출력하는 것이 완벽히 익숙하진 않지만..
+그래도 좀 늘었..다고 할 수 있겠지?
+
+그리고 하나 더 해볼것!
+%c를 통해 한 줄 씩 저장하는 것은 성공했는데,
+%s도 이제는 시도해봐도 될 듯? 안된다면 안되는 이유를 이제는 이해할 수 있을 듯.
 */
